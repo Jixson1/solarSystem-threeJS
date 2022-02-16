67c48a71de4b89ef1d2ff07ed3f71f067630b9bb
Jackson Buchmeyer <jackson.buchmeyer@drake.edu>
2022-02-15 20:51:34 -0600

    correct formating

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
@@ -16,7 +15,7 @@ class planet {
 // initializing scene and camera
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 4000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 4000);
 
 // renderer boilerplate
 const renderer = new THREE.WebGLRenderer({
@@ -33,69 +32,69 @@ camera.position.set(0, 60, 120);
 let planetArr = [];
 
 // sun
-const sun = new planet(0, 80, 0, .01*(1/27), 
+const sun = new planet(0, 80, 0, .01 * (1 / 27),
   new THREE.Mesh(
-    new THREE.SphereGeometry( 40, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/sun.jpg') } )
+    new THREE.SphereGeometry(40, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/sun.jpg') })
   )
 );
 scene.add(sun.mesh);
 
 // earth
-const earth = new planet(.0001, 2, sun.radius+92.96, .01,
+const earth = new planet(.0001, 2, sun.radius + 92.96, .01,
   new THREE.Mesh(
-    new THREE.SphereGeometry( 2, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/earth.jpg') } )
+    new THREE.SphereGeometry(2, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/earth.jpg') })
   )
 );
 earth.mesh.position.setX(earth.orbitRadius);
 scene.add(earth.mesh);
 
 // mercury
-const mercury = new planet(earth.speed*(1/.241), earth.radius*(2/5), sun.radius+35.98, earth.rotation*(1/59),
+const mercury = new planet(earth.speed * (1 / .241), earth.radius * (2 / 5), sun.radius + 35.98, earth.rotation * (1 / 59),
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*(2/5), 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/mercury.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * (2 / 5), 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/mercury.jpg') }),
   )
 );
 mercury.mesh.position.setX(mercury.orbitRadius);
 scene.add(mercury.mesh);
 
 // venus
-const venus = new planet(earth.speed*(1/.616), earth.radius*.949, sun.radius+67.24, earth.rotation*(1/117),
+const venus = new planet(earth.speed * (1 / .616), earth.radius * .949, sun.radius + 67.24, earth.rotation * (1 / 117),
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*.949, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/venus.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * .949, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/venus.jpg') }),
   )
 );
 venus.mesh.position.setX(venus.orbitRadius);
 scene.add(venus.mesh);
 
 // mars
-const mars = new planet(earth.speed*(1/1.88), earth.radius*.532, sun.radius+141.6*.75, earth.rotation*(1/1.026),
+const mars = new planet(earth.speed * (1 / 1.88), earth.radius * .532, sun.radius + 141.6 * .75, earth.rotation * (1 / 1.026),
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*.532, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/mars.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * .532, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/mars.jpg') }),
   )
 );
 mars.mesh.position.setX(mars.orbitRadius);
 scene.add(mars.mesh);
 
 // jupiter
-const jupiter = new planet(earth.speed*(1/12), earth.radius*11, sun.radius+483.8*.3, earth.rotation*2.4,
+const jupiter = new planet(earth.speed * (1 / 12), earth.radius * 11, sun.radius + 483.8 * .3, earth.rotation * 2.4,
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*11, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/jupiter.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * 11, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/jupiter.jpg') }),
   )
 );
 jupiter.mesh.position.setX(jupiter.orbitRadius);
 scene.add(jupiter.mesh);
 
 // saturn
-const saturn = new planet(earth.speed*(1/29), earth.radius*9.45, sun.radius+890.8*.3, earth.rotation*2.24,
+const saturn = new planet(earth.speed * (1 / 29), earth.radius * 9.45, sun.radius + 890.8 * .3, earth.rotation * 2.24,
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*9.45, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/saturn.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * 9.45, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/saturn.jpg') }),
   )
 );
 saturn.mesh.position.setX(saturn.orbitRadius);
@@ -106,44 +105,44 @@ ringsTexture.repeat.set(.5, .5);
 ringsTexture.offset.set(.2, .2);
 const saturnRing = new THREE.Mesh(
   new THREE.RingGeometry(saturn.radius + 4, saturn.radius + 8, 50, 50),
-  new THREE.MeshBasicMaterial( { map: ringsTexture, side: THREE.DoubleSide } )
+  new THREE.MeshBasicMaterial({ map: ringsTexture, side: THREE.DoubleSide })
 );
-saturnRing.rotateX(-Math.PI/2.5);
+saturnRing.rotateX(-Math.PI / 2.5);
 saturn.mesh.add(saturnRing);
 
 // uranus
-const uranus = new planet(earth.speed*(1/84), earth.radius*4.007, sun.radius+1784*.3, earth.rotation*1.39,
+const uranus = new planet(earth.speed * (1 / 84), earth.radius * 4.007, sun.radius + 1784 * .3, earth.rotation * 1.39,
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*4.007, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/uranus.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * 4.007, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/uranus.jpg') }),
   )
 );
 uranus.mesh.position.setX(uranus.orbitRadius);
 scene.add(uranus.mesh);
 
 // neptune
-const neptune = new planet(earth.speed*(1/65), earth.radius*3.883, sun.radius+2793*.3, earth.rotation*1.49,
+const neptune = new planet(earth.speed * (1 / 65), earth.radius * 3.883, sun.radius + 2793 * .3, earth.rotation * 1.49,
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*3.883, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/neptune.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * 3.883, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/neptune.jpg') }),
   )
 );
 neptune.mesh.position.setX(neptune.orbitRadius);
 scene.add(neptune.mesh);
 
 // pluto
-const pluto = new planet(earth.speed*(1/248), earth.radius*.186*10, sun.radius+3700*.3, earth.rotation*(1/6.4),
+const pluto = new planet(earth.speed * (1 / 248), earth.radius * .186 * 10, sun.radius + 3700 * .3, earth.rotation * (1 / 6.4),
   new THREE.Mesh(
-    new THREE.SphereGeometry( earth.radius*.186*10, 100, 100 ),
-    new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('textures/pluto.jpg') } ),
+    new THREE.SphereGeometry(earth.radius * .186 * 10, 100, 100),
+    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('textures/pluto.jpg') }),
   )
 );
 pluto.mesh.position.setX(pluto.orbitRadius);
 scene.add(pluto.mesh);
 
 planetArr.push(
-  sun, mercury, venus, earth, mars, 
-  jupiter, saturn, uranus, neptune, 
+  sun, mercury, venus, earth, mars,
+  jupiter, saturn, uranus, neptune,
   pluto
 )
 
@@ -154,10 +153,10 @@ for (let i = 1; i < planetArr.length; i += 1) {
 
 // lighting
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add( ambientLight );
+scene.add(ambientLight);
 
 // orbit controls
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.maxDistance = 2000;
 
 // textures and texture mapping
@@ -172,22 +171,22 @@ renderer.domElement.addEventListener('pointerdown', raycast, false);
 // button click handling
 let buttons = document.getElementsByTagName('button');
 for (var i = 0; i < buttons.length; i++) {
-    buttons[i].onclick = function (){
-        buttonPress(this);
-    }
+  buttons[i].onclick = function () {
+    buttonPress(this);
+  }
 }
 
 function buttonPress(button) {
   switch (button.id) {
-    case '1': changePivot(mercury.mesh); break; 
-    case '2': changePivot(venus.mesh); break; 
+    case '1': changePivot(mercury.mesh); break;
+    case '2': changePivot(venus.mesh); break;
     case '3': changePivot(earth.mesh); break;
-    case '4': changePivot(mars.mesh); break; 
-    case '5': changePivot(jupiter.mesh); break; 
-    case '6': changePivot(saturn.mesh); break; 
-    case '7': changePivot(uranus.mesh); break; 
-    case '8': changePivot(neptune.mesh); break; 
-    case '9': changePivot(pluto.mesh); break; 
+    case '4': changePivot(mars.mesh); break;
+    case '5': changePivot(jupiter.mesh); break;
+    case '6': changePivot(saturn.mesh); break;
+    case '7': changePivot(uranus.mesh); break;
+    case '8': changePivot(neptune.mesh); break;
+    case '9': changePivot(pluto.mesh); break;
     case '0': changePivot(sun.mesh); break;
   }
 };
@@ -197,14 +196,14 @@ function raycast(event) {
   event.preventDefault();
   //1. sets the mouse position with a coordinate system where the center
   //   of the screen is the origin
-  mouseVector = new THREE.Vector3( ( event.clientX / window.innerWidth ) * 2 - 1, - ( event.clientY / window.innerHeight ) * 2 + 1, .5 );
+  mouseVector = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1, - (event.clientY / window.innerHeight) * 2 + 1, .5);
 
   //2. set the picking ray from the camera position and mouse coordinates
   raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera( mouseVector, camera );
+  raycaster.setFromCamera(mouseVector, camera);
 
   //3. compute intersections
-  let intersects = raycaster.intersectObjects( scene.children );
+  let intersects = raycaster.intersectObjects(scene.children);
   if (intersects.length > 0 && intersects[0].object.type == 'Mesh') {
     if (pivot != intersects[0].object) {
       changePivot(intersects[0].object);
@@ -224,9 +223,9 @@ function changePivot(planetMesh) {
 function drawOrbit(planet) {
   let pts = new THREE.Path().absarc(0, 0, planet.orbitRadius, 0, Math.PI * 2).getPoints(90);
   let g = new THREE.BufferGeometry().setFromPoints(pts);
-  let m = new THREE.LineBasicMaterial( { color: 0xffffff, transparent: true, opacity: 0.2 } );
+  let m = new THREE.LineBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.2 });
   let l = new THREE.Line(g, m)
-  l.rotateX(Math.PI/2);
+  l.rotateX(Math.PI / 2);
   scene.add(l);
 }
 
@@ -255,39 +254,39 @@ function orbit(planet) {
 }
 
 // resize window
-window.addEventListener('resize', onWindowResize, false ); 
+window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize (window.innerWidth, window.innerHeight);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 // animate function
 // this is where the magic happens
 function animate() {
-    requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 
-    // planet rotation and orbit
-    for (let i = 0; i < planetArr.length; i += 1) {
-      rotatePlanet(planetArr[i]);
-      orbit(planetArr[i]);
-    }
+  // planet rotation and orbit
+  for (let i = 0; i < planetArr.length; i += 1) {
+    rotatePlanet(planetArr[i]);
+    orbit(planetArr[i]);
+  }
 
-    // camera orbit controls
-    if (pivot != null) {
-      controls.target = pivot.position;
-      let cameraDistance = Math.sqrt(Math.pow(camera.position.x - pivot.position.x, 2) 
-      + Math.pow(camera.position.y - pivot.position.y, 2) 
+  // camera orbit controls
+  if (pivot != null) {
+    controls.target = pivot.position;
+    let cameraDistance = Math.sqrt(Math.pow(camera.position.x - pivot.position.x, 2)
+      + Math.pow(camera.position.y - pivot.position.y, 2)
       + Math.pow(camera.position.z - pivot.position.z, 2))
-      if (cameraDistance > (80 * pivot.geometry.boundingSphere.radius / 20) && cameraAtPlanet == false) {
-        camera.translateZ(-1 * cameraDistance/150);
-      } else {
-        cameraAtPlanet = true;
-      }
+    if (cameraDistance > (80 * pivot.geometry.boundingSphere.radius / 20) && cameraAtPlanet == false) {
+      camera.translateZ(-1 * cameraDistance / 150);
+    } else {
+      cameraAtPlanet = true;
     }
-    controls.update();
+  }
+  controls.update();
 
-    renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 animate()
